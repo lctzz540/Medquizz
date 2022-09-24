0795b7a9c84f250b822a6090a127bda845710043
Thang Le <lctzz540@outlook.com>
2022-09-25 01:21:09 +0700

    first commit

--- quiz-app/final/src/data/questions.js
+++ quiz-app/final/src/data/questions.js
@@ -2,12 +2,12 @@ import daicuong from './Đại cương.json'
 import giaibieu from './Giải biểu.json'
 import taha from './Tả hạ.json'
 import phuongthuocsonggiaibieuly from './Phương-thuốc-song-giải-biểu-lý.json'
-import phuongthuocthanhnhiet from './Phương-thuốc-thanh-nhiệt-trừ-thử.json'
+import thanhnhiet from './Thanh-nhiệt.json'
 
 export const questions = [].concat(
   daicuong,
   giaibieu,
   taha,
   phuongthuocsonggiaibieuly,
-  phuongthuocthanhnhiet
+  thanhnhiet
 )
