6543a0ece90eb3fde6cd0e9d4da09bde606265e4
Thang Le <lctzz540@outlook.com>
2022-09-29 22:01:32 +0700

    lastest commit

--- backend/api.py
+++ backend/api.py
@@ -4,22 +4,6 @@ from controller import processFileUpload
 
 app = FastAPI()
 
-class answer_class:
-    def __init__(self, answerText, isCorrect):
-        self.answerText = answerText
-        self.isCorrect = isCorrect
-class question_class:
-    def __init__(self, questionText, questionID):
-        self.questionText = questionText
-        self.answerOptions = []
-        self.questionID = questionID
-class case_class:
-    def __init__(self, question_group, question_group_text):
-        self.question_group = question_group
-        self.question_group_text = question_group_text
-        self.ques = []
-
-
 @app.get("/")
 def hello():
     return 'This app is running'
--- backend/database.py
+++ backend/database.py
@@ -1,3 +1,5 @@
-client = pymongo.MongoClient("mongodb+srv://lctzz540:Thang23062001@examapp.zow8dzs.mongodb.net/?retryWrites=true&w=majority")
+from pymongo import MongoClient
+
+client = MongoClient("mongodb+srv://lctzz540:Thang23062001@examapp.zow8dzs.mongodb.net/?retryWrites=true&w=majority")
 db = client.test
 collection_name = db["Question"]
